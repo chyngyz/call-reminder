81f0f90337904ad6d06b799c5336c6c95f354030
chyngyz <arystanuulu@gmail.com>
2020-02-23 22:51:46 +0600

    Updatye

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Elevation, Dialog, Classes, Intent, RadioGroup, Radio } from "@blueprintjs/core";
+import AddNewContact from './AddNewContact'
 
 function App() {
+  const [contacts, setContacts] = useState([
+    {
+      name: 'Chyngyz',
+      frequency: 'daily',
+      reminderTime: '20:13',
+      date: new Date()
+    }
+  ])
+
+  useEffect(() => {
+    const savedContacts = window.localStorage.getItem('contacts')
+    if (savedContacts && savedContacts.length) {
+      setContacts(savedContacts)
+    }
+  }, [])
+
+  const handleSave = (contact) => {
+    setContacts([
+      ...contacts,
+      contact
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1>Call Reminder</h1>
       </header>
+      <main>
+        {contacts.map(contact => (
+          <Card elevation={Elevation.TWO} className="contact-card">
+            <h2>Call <i>{contact.name}</i></h2>
+            <p>Your call schedule is <b>{contact.frequency}</b> at <b>{contact.reminderTime}</b></p>
+
+            <div className="contact-card__actions">
+              <Button
+                text="Called"
+                intent={Intent.PRIMARY}
+              />
+
+              <Button
+                text="Missed"
+                intent={Intent.DANGER}
+              />
+            </div>
+          </Card>
+        ))}
+      </main>
+      <footer>
+        <AddNewContact
+          onSave={handleSave}
+        />
+      </footer>
     </div>
   );
 }
